f9803790b47428f494b1aa9ab6aad461694ddb44
Jonathan Armentor <jonathan@comitdevelopers.com>
2024-11-24 13:05:50 -0600

    update neo-tree behavior and excludes

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -13,11 +13,26 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        never_show = {
+          '.DS_Store',
+        },
+        always_show_by_pattern = {
+          '.env*',
+        },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
       },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      hijack_netrw_behavior = 'open_default',
     },
   },
   config = function(_, opts)
