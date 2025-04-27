8da402234bd2171c874ecbd262a36d8a7ef19a5a
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2025-04-27 07:23:52 -0500

    statusline modifications

--- nvim/lua/apiarist/plugins/mini.lua
+++ nvim/lua/apiarist/plugins/mini.lua
@@ -28,14 +28,16 @@ return { -- Collection of various small independent plugins/modules
     -- Replace Comment.nvim with mini.comment for commenting support
     require('mini.comment').setup()
 
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-
-    -- cursor location to LINE:COLUMN
+    local ms = require 'mini.statusline'
+    -- Custom location format: LINE:COLUMN with fixed width
     ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
+    ms.section_location = function()
       return '%2l:%-2v'
     end
+
+    ms.setup {
+      use_icons = vim.g.have_nerd_font,
+    }
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
