92fafd359497a4727d5cdd92196fecc36e1c49a7
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2025-07-24 08:37:28 -0500

    better markdown wrapping pls aaa

--- nvim/lua/apiarist/autocommands.lua
+++ nvim/lua/apiarist/autocommands.lua
@@ -154,3 +154,15 @@ vim.api.nvim_create_autocmd('CursorHold', {
     end
   end,
 })
+
+-- Better word wrapping for markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.breakindent = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.textwidth = 0
+    vim.opt_local.wrapmargin = 0
+  end,
+})
