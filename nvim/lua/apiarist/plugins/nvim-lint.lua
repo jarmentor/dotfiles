f4e0dfdd306a421c4acbdc1419371bde2497fe71
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2025-09-04 05:49:14 -0500

    enhancement: diagnostics / linting floats

--- nvim/lua/apiarist/plugins/nvim-lint.lua
+++ nvim/lua/apiarist/plugins/nvim-lint.lua
@@ -9,6 +9,7 @@ return {
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
         json = { 'jsonlint' },
+        php = { 'phpcs' },
       }
 
       -- Create autocommand to run linting on BufEnter, BufWritePost, InsertLeave
@@ -24,6 +25,27 @@ return {
           end
         end,
       })
+
+      -- Show diagnostics in hover popup on cursor hold
+      vim.api.nvim_create_autocmd('CursorHold', {
+        group = lint_augroup,
+        callback = function()
+          -- Only show hover if there are diagnostics on the current line
+          local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
+          if #diagnostics > 0 then
+            vim.diagnostic.open_float(nil, {
+              focusable = false,
+              close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+              border = 'rounded',
+              source = 'always',
+              prefix = ' ',
+            })
+          end
+        end,
+      })
+
+      -- Set updatetime for faster CursorHold trigger (default is 4000ms)
+      vim.opt.updatetime = 1000
     end,
   },
 }
