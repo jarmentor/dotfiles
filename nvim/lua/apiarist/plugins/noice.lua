bcafbb2fe043def8db7db32fff680bdafc1270d7
Jonathan Armentor <jonathan@jonathanarmentor.dev>
2025-09-10 05:14:50 -0500

    add noice.nvim for prettier command input

---
+++ nvim/lua/apiarist/plugins/noice.lua
@@ -0,0 +1,57 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    {
+      "rcarriga/nvim-notify",
+      opts = {
+        background_colour = "#000000",
+      }
+    }
+  },
+  config = function()
+    -- Disable lazyredraw to avoid conflicts with noice
+    vim.opt.lazyredraw = false
+    
+    require("noice").setup({
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "50%",
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = "auto",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+            row = "60%",
+            col = "50%",
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    })
+  end,
+}
